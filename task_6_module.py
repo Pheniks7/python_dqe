d018651c05326d2d5ab723c1e8262c6f2c9ffc25
Sergei_Kiriushkin <pheniks-7@mail.ru>
2022-11-06 20:02:24 +0300

    add run task_5

---
+++ task_6_module.py
@@ -0,0 +1,8 @@
+from task_5_classes import NewsFeed
+
+if __name__ == "__main__":
+    task_5 = NewsFeed()
+    task_5.add_content_manually()
+    task_5.add_content_manually()
+    task_5.add_content_manually()
+    task_5.show_news_feed()
