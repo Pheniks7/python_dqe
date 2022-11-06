6b5435d03ed1eb2fe7d2d228563fc449945dcd96
Sergei_Kiriushkin <pheniks-7@mail.ru>
2022-11-06 20:00:14 +0300

    add input parameters to init in task_5_classes.py

--- task_5_classes.py
+++ task_5_classes.py
@@ -1,29 +1,37 @@
 from datetime import datetime, timedelta
 from random import randrange
 
+from task_3_string import capitalize_first_words
+
+
+def create_record_lines(class_name, text_to_publish):
+    return (f'\n{class_name.ljust(35, "-")}\n'
+            f'{text_to_publish}\n'
+            f'-----------------------------------\n')
+
 
 class News:
-    def __init__(self):
+    def __init__(self, txt=None, city=None):
         self.cur_time = datetime.now()
-        self.txt = input('\nType the news: ').capitalize()
-        self.city = input('Enter the city: ').title()
+        self.txt = capitalize_first_words(input('\nType the news: ') if txt is None else txt)
+        self.city = (input('Enter the city: ').title() if city is None else city).title()
 
-    def publish(self):
-        return (f'News ------------------------------\n'
-                f'{self.txt}\n{self.city}, {self.cur_time.strftime("%d/%m/%Y %H.%m")}'
-                f'\n-----------------------------------\n')
+    def create_record(self):
+        text_to_publish = f'{self.txt}\n{self.city}, {self.cur_time.strftime("%d/%m/%Y %H.%m")}'
+        return create_record_lines(__class__.__name__, text_to_publish)
 
 
 class PrivateAd:
-    def __init__(self):
-        self.txt = input('\nType the advertisement: ').capitalize()
-        self.date = self.__check_input_date()
+    def __init__(self, txt=None, date=None):
+        self.txt = capitalize_first_words(input('\nType the advertisement: ') if txt is None else txt)
+        self.date = self.__check_input_date(date)
 
-    def __check_input_date(self):
+    def __check_input_date(self, date=None):
         for i in range(3):
             try:
-                input_date = datetime.strptime(input('Enter the ad expiration date in this formate "dd/mm/yy": '),
-                                               '%d/%m/%y')
+                input_date_raw = input(
+                    'Enter the ad expiration date in this formate "dd/mm/yy": ') if date is None else date
+                input_date = datetime.strptime(input_date_raw, '%d/%m/%y')
                 date_diff = int((input_date - datetime.now()).days)
                 if date_diff < 0 or date_diff > 365:
                     raise ValueError('\nPlease, enter valid date')
@@ -35,53 +43,48 @@ class PrivateAd:
                 input_date = datetime.now() + timedelta(days=15)
                 return input_date
 
-    def publish(self):
+    def create_record(self):
         date_diff = self.date - datetime.now()
-        return (f'Private Ad ------------------------\n'
-                f'{self.txt}\nActual until: {self.date.strftime("%d/%m/%Y")}, {date_diff.days} days left'
-                f'\n-----------------------------------\n')
+        text_to_publish = f'{self.txt}\nActual until: {self.date.strftime("%d/%m/%Y")}, {date_diff.days} days left'
+        return create_record_lines(__class__.__name__, text_to_publish)
 
 
 class Joke:
-    __JOKE_METER_LIST = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 
-    def __init__(self):
-        self.txt = input('\nType the joke: ').capitalize()
+    def __init__(self, txt=None):
+        self.txt = capitalize_first_words(input('\nType the joke: ') if txt is None else txt)
         self.rate = self.__rate_joke()
 
     def __rate_joke(self):
-        return f'{Joke.__JOKE_METER_LIST[randrange(10)]} of ten'
+        joke_meter_list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+        return f'{joke_meter_list[randrange(10)]} of ten'
 
-    def publish(self):
-        return (f'Joke of the day -------------------\n'
-                f'{self.txt}\nFunny meter - {self.rate}'
-                f'\n-----------------------------------\n')
+    def create_record(self):
+        text_to_publish = f'{self.txt}\nFunny meter - {self.rate}'
+        return create_record_lines(__class__.__name__, text_to_publish)
 
 
 class NewsFeed:
-    __OPTION = {'1': 'News()', '2': 'PrivateAd()', '3': 'Joke()'}
 
     def __init__(self):
-        self.news_feed = ['News feed:\n']
+        # self.news_feed = ['News feed:\n']
+        self.news_feed = 'News feed:\n'
 
-    def add_content(self):
+    def add_content_manually(self):
         while True:
             option = input('Choose one of the following options You want to add in NewsFeed:\n'
                            '1 - News\n2 - PrivateAd\n3 - Joke\nYour choice: ')
             if option in ['1', '2', '3']:
                 break
             print('\nIncorrect input. Try again!\n')
-        content = eval(NewsFeed.__OPTION[option]).publish()
-        self.news_feed.append(content)
+        if option == '1':
+            content = News().create_record()
+        elif option == '2':
+            content = PrivateAd().create_record()
+        else:
+            content = Joke().create_record()
+        self.news_feed = self.news_feed + content
         print('\nNew content was added\n')
 
     def show_news_feed(self):
-        for elem in self.news_feed:
-            print(elem)
-
-
-my_news_feed = NewsFeed()
-my_news_feed.add_content()
-my_news_feed.add_content()
-my_news_feed.add_content()
-my_news_feed.show_news_feed()
+        print(self.news_feed)
