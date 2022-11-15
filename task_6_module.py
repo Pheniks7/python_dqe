34a9f1e7ae5f2e58e0ace1c94f29219a5f0eea42
Sergei_Kiriushkin <pheniks-7@mail.ru>
2022-11-15 07:56:30 +0400

    simplify task_6_module.py

--- task_6_module.py
+++ task_6_module.py
@@ -1,13 +1,4 @@
-from task_5_classes import NewsFeed
+from task_5_classes_wo_init import NewsFeed
 
 if __name__ == "__main__":
-    # task_5 = NewsFeed()
-    # task_5.add_content_manually()
-    # task_5.add_content_manually()
-    # task_5.add_content_manually()
-    # task_5.show_news_feed()
-    # task_5.save_as_file()
-    task_6 = NewsFeed()
-    check_point = task_6.read_file()
-    if check_point is not None:
-        task_6.save_as_file()
+    NewsFeed().choose_options()
