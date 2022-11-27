702a5c76bfc21b27d902c68f72161be78b923757
Sergei_Kiriushkin <pheniks-7@mail.ru>
2022-11-27 09:09:33 +0400

    update read file path methods

--- file_worker.py
+++ file_worker.py
@@ -1,20 +1,22 @@
+import os
 import re
 
 
-def check_path(raw_input_text):
-    input_text = raw_input_text.strip()
-    input_text_format = input_text if re.search('.txt$',
-                                                input_text) or re.search('.doc$',
-                                                                         input_text) else input_text + '.txt'
-    file_path = input_text_format if re.search('^C:', input_text_format) else 'txt_files\\' + input_text_format
-    return file_path
-
-
-def get_full_path():
-    input_name_dir = input('Input the file_name (with its path - optional)\n'
-                           'Note: to skip this step - press Enter button\n-- ')
-    if re.search('[\w]+', input_name_dir):
-        full_path = check_path(input_name_dir)
+def get_read_path():
+    input_file_name = input('Input the file_name\n'
+                            'Note: to skip this step - press Enter button\n-- ')
+    input_name_dir = os.path.join('txt_files', input_file_name)
+    if os.path.exists(input_name_dir) and os.path.isfile(input_name_dir):
+        return input_name_dir
     else:
-        full_path = 'txt_files\\No_name_text_file.txt'
-    return full_path
+        raise FileNotFoundError
+
+
+def get_write_path():
+    output_file_name = input('Input the file_name\n'
+                             'Note: to skip this step - press Enter button\n-- ')
+    output_name_dir = os.path.join('txt_files', output_file_name)
+    # if not re.search('[\w]+.txt$', output_name_dir) and not re.search('[\w]+.doc$', output_name_dir):
+    if not os.path.isfile(output_name_dir):
+        output_name_dir = os.path.join('txt_files', 'No_name_text_file.txt')
+    return output_name_dir
